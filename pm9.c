a6a7da8b25697258b4c4f02ceed28e780a5c6871
7373462212 <31505951+7373462212@users.noreply.github.com>
2017-09-02 12:17:59 +0530

    Create pm9.c

---
+++ pm9.c
@@ -0,0 +1,14 @@
+#nclude<iostream.h>
+#include<conio.h>
+int main()
+{
+int p,i,n;
+printf("Enter thenumber");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+p=p+i;
+printf("Sum of N numbers is p=  ",p);
+}
+return 0;
+}
