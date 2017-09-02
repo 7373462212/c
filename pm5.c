4231d67cca042332a886d83f5c0abee06f1c8c42
7373462212 <31505951+7373462212@users.noreply.github.com>
2017-09-02 11:54:42 +0530

    Create pm5.c

---
+++ pm5.c
@@ -0,0 +1,21 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+printf("Enter the number ");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b&&a>c)
+{
+printf("The largest number is a=  ",a);
+}
+elseif(b>a&&b>C)
+{
+printf("The largest number is b= ",b);
+}
+else
+{
+printf("The largest number is c= ",c);
+}
+return 0;
+}
