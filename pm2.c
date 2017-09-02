6e3daa3b79be95061cc08ac23905849d40e71d23
7373462212 <31505951+7373462212@users.noreply.github.com>
2017-09-02 11:42:10 +0530

    Create pm2.c

---
+++ pm2.c
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("Enter the number ");
+scanf("%d%,&n);
+if(n%2==0)
+{
+printf("Given number is even");
+}
+else
+{
+printf("Given number is odd");
+}
+return 0;
+}
