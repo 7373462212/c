451a5cc41281ef110d2f277b3b16e6f05ceb7eb6
7373462212 <31505951+7373462212@users.noreply.github.com>
2018-01-08 16:31:52 +0530

    Create prm3.c

---
+++ prm3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("Enter any alphabet");
+scanf("%c",&ch);
+if(ch=='a'||ch='e'||ch='i'||ch='o'ch='u'||ch='A'||ch='E'||ch='I'||ch='O'||ch='U')
+{
+printf("\nGiven alphabet is Vowel ch=%c",ch);
+}
+else
+{
+printf("\nGiven alphabet is Consonant ch=%c",ch);
+}
+getch();
+}
