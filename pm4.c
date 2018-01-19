c3d5f8b644f6b9283042fe7de12eb7fa416aace9
7373462212 <31505951+7373462212@users.noreply.github.com>
2018-01-19 15:57:56 +0530

    Create pm4.c

---
+++ pm4.c
@@ -0,0 +1,17 @@
+#nclude<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("\nEnter your choice");
+scanf("%c",&ch);
+if(ch>=65&&ch<=90||ch>=97&&ch<==122)
+{
+printf("\n ALPHABET");
+}
+else
+{
+printf("\nNUMBER");
+}
+getch();
+}
