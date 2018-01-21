1793305c7e04335384da093656089264c40fbd8f
7373462212 <31505951+7373462212@users.noreply.github.com>
2018-01-21 11:23:04 +0530

    Create pm18.c

---
+++ pm18.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int num,r,temp,sum=0,a=1,b=50;
+printf("enter an number: ");
+scanf("%d",&num);
+temp=num;
+if(a<=b)
+{
+while(num!=0)
+{
+r=num%10;
+num=num/10;
+sum=sum+(r*r*r);
+}
+}
+if(sum==temp)
+{
+printf("%d is a armstrong",temp);
+}
+else
+{
+printf("%d ia not an arnstrong",temp);
+}
+return 0;
+}
