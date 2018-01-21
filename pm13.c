8eb2fec488af48a409c3131f71d26736d543554c
7373462212 <31505951+7373462212@users.noreply.github.com>
2018-01-21 11:18:59 +0530

    Create pm13.c

---
+++ pm13.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void) {
+	int i,c,n,a=1,b=10;
+	printf("\nEnter the number");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		c++;
+	}
+	if(c==2)
+	{
+		printf("\nThe prime number is %d",c);
+	}
+	else
+	{
+		printf("\nThere is no prime number");
+	}
+	return 0;
+	}
