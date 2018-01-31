8a7d34b3681f306ce8d9385f5ec22872f10a8a04
7373462212 <31505951+7373462212@users.noreply.github.com>
2018-01-31 16:06:57 +0530

    Update pm22.c

--- pm22.c
+++ pm22.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 int main(void) {
-	int a[10],n,i;
-	printf("Enter the number");
-	scanf("%d",&n);
-	for(i=0;i<=n;i++)
+	int a[3],i,j,b;
+	printf("\nEnter the elements");
+	for(i=0;i<3;i++)
 	{
-		scanf("%d",&a[i]);
+	    scanf("%d",&a[i]);
 	}
-
+	for(i=0;i<3;i++)
+	{
+	 for(j=i+1;j<3;j++)
+     {
+    
+	if(a[i]<a[j])
+	{
+		b=a[j];
+		a[j]=a[i];
+		a[i]=b;
+	}
+    }
+	}
+	printf("\nThe maximum element is:%d",a[0]);
 	return 0;
 }
