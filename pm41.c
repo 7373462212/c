0b029603ffaf396b6e8346b8f4db0094b739c90a
7373462212 <31505951+7373462212@users.noreply.github.com>
2018-02-02 17:01:26 +0530

    Create pm41.c

---
+++ pm41.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void){
+    int n;
+    char ch[80],i;
+    printf("\nEnter the number:");
+    scanf("%d",&n);
+    printf("\nEnter the characters:");
+    scanf("%s",&ch);
+    for(i=0;i<=n;i++)
+    {
+        printf("%s",ch);
+    }
+    return 0;
+    }
