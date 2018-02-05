bc3db6d70603ec0d82ec0a60304c33818a76db2c
7373462212 <31505951+7373462212@users.noreply.github.com>
2018-02-05 16:53:43 +0530

    Create pm45.c

---
+++ pm45.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void){
+    int a[3],i,n,count=0;
+    printf("\nenter the numbers:");
+    for(i=0;i<n;i++)
+    {
+    scanf("%d",&a[i]);
+    count++;
+    }
+    printf("\nThe number of numeric digits is:%d",count);
+    return 0;
+}
