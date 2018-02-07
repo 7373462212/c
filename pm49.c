be09c678a526cf4d9da2e2858aab5f93202a661b
7373462212 <31505951+7373462212@users.noreply.github.com>
2018-02-07 16:15:48 +0530

    Create pm49.c

---
+++ pm49.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("\nEnter first number:");
+    scanf("%d",&a);
+    printf("\nEnter the second number:");
+    scanf("%d",&b);
+    c=a+b;
+    printf("\nThe sum of two number is:%d",c);
+    return 0;
+}
