ac50ba0ecc0cd713c4a4b6268daa64918d438e5e
7373462212 <31505951+7373462212@users.noreply.github.com>
2018-02-09 16:06:14 +0530

    Create pm59.c

---
+++ pm59.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],i,j,b;
+    printf("\nEnter the number :");
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<10;i++)
+    {
+        for(j=i+1;j<10;j++)
+        {
+            if(a[i]<a[j])
+            {
+              b=a[i];
+              a[i]=a[j];
+              a[j]=b;
+            }
+        }
+    }
+printf("\nThe maximum number is %d",a[0]);
+return 0;
+}
