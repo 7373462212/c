18da4ebbc1577ab274f73bedc6761f09688d374a
7373462212 <31505951+7373462212@users.noreply.github.com>
2018-02-12 16:58:47 +0530

    Create pm34.c

---
+++ pm34.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char a[10];
+    int i,count=0;
+    printf("\nEnter the character:");
+    gets(a);
+    for(i=0;a[i]!=NULL;i++)
+    {
+        if(a[i]=='.')
+        {
+            count++;
+        }
+    }
+    printf("%d",count+1);
+    return 0;
+    }
