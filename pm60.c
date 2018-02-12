fb6f1678d01a4bd3775c89d92e80e5be801607ba
7373462212 <31505951+7373462212@users.noreply.github.com>
2018-02-12 16:36:14 +0530

    Update pm60.c

--- pm60.c
+++ pm60.c
@@ -1,18 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c,i,n;
-    printf("\nEnter the number:");
+    int i,n,nt,x,y;
+    printf("\nEnter the number :");
     scanf("%d",&n);
-    a=0;
-    b=1;
-    c=0;
     for(i=0;i<n;i++)
     {
-        a=b;
-        b=c;
-        c=a+b;
-        printf("%d",c);
+        printf("%d",x);
+        nt=x+y;
+        x=y;
+        y=nt;
     }
     return 0;
 }
