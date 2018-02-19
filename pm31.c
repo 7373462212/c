a57cd4107edb238af7c18e331cb8a688b20fcaa1
7373462212 <31505951+7373462212@users.noreply.github.com>
2018-02-19 16:23:19 +0530

    Create pm31.c

---
+++ pm31.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void){
+char a[20];
+int l,i;
+printf("\nEnter the characters:");
+scanf("%s",a[i]);
+while(i!='\0')
+{
+if((a[i]>='a')&&(a[i]<='z'))
+{
+l=strlen(a);
+}
+i++;
+}
+printf("\n%d",l);
+return 0;
+}
