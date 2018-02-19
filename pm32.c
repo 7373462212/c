3a599a9c3344cbf695fc445df4bebeea05a32c7b
7373462212 <31505951+7373462212@users.noreply.github.com>
2018-02-19 16:36:59 +0530

    Create pm32.c

---
+++ pm32.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+	char ch[100];
+	int m,i=0,count=1;
+	scanf("%[^\t\n]s",a);
+	n=strlen(a);
+	while(i<n)
+	{
+	   if(a[i]==' ')
+	    {
+	        count++;
+	    }
+      i++;
+	}
+	printf("%d",count);
+	return 0;
+}
+	    
