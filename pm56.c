f5b802632e4b73474626d56ea9228e4a4d9597bf
7373462212 <31505951+7373462212@users.noreply.github.com>
2018-02-19 16:32:43 +0530

    Create pm56.c

---
+++ pm56.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char a[30],i;
+printf("\nEnter the string:");
+scanf("%[^\n]s",a[i]);
+if((a[i]>='a'&&a[i]<='z')&&a[i]>='0'&&a[i]<='9'))
+{
+printf("YES");
+}
+else
+{
+printf("NO");
+}
+return 0;
+}
