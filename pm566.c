3ea6a98e8ae59159c1f61397883c817c09c8e7b9
7373462212 <31505951+7373462212@users.noreply.github.com>
2018-02-28 16:30:45 +0530

    Rename pm56.c to pm566.c

---
+++ pm566.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(void){
+    char ch[30],i,temp=0;
+    int count=0;
+    printf("\nEnter the sentence:");
+    scanf("%c",&ch);
+    if((ch[i]<='a')||(ch[i]>='z'))
+    {
+        temp++;
+    }
+    if((ch[i]>='0')||(ch[i]<='9'))
+    {
+        count++;
+    }
+    if(temp>=count)
+    {
+        printf("YES,It contain more number of characters");
+    }
+    else if(count>=temp)
+    {
+        printf("YEs,It contain more number of numbers");
+    }
+    else
+    {
+        printf("There is no input");
+    }
+    return 0;
+}
