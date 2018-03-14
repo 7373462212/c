7826824b9fb1e4a704d4a6f726cb292c049a21bd
7373462212 <31505951+7373462212@users.noreply.github.com>
2018-03-14 16:49:44 +0530

    Create pm90.c

---
+++ pm90.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char ch[10];
+    int i;
+    printf("Enter the string:");
+    scanf("%s",ch);
+    for(i=0;ch[i]!='\0';i++)
+    {
+        if(isdigit(ch[i]))
+        {
+            printf("%c",ch[i]);
+        }
+    }
+    return 0;
+}
