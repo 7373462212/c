f4bdab8b3bb78e831c0d756f0111c8a709fe8d31
7373462212 <31505951+7373462212@users.noreply.github.com>
2018-03-19 16:42:40 +0530

    Update pm96.c

--- pm96.c
+++ pm96.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
-void main()
+int main()
 {
 int n,count=0,i;
 printf("Enter the number:");
 scanf("%d",&n);
-for(i=2;i<n;i++)
-{
-if((n%i)==0)
-{
-count++;
-}
-}
-if(count==0)
-{
-printf("No");
-}
-else
-{
-printf("Yes");
-}
+  for(i=2;i<n;i++)
+  {
+    if(n%i==0)
+   {
+     count++;
+    }
+   }
+  if(count==0)
+  {
+    printf("No");
+   }
+  else
+  {
+   printf("Yes");
+   }
+   return 0;
 }
